a23339a6c5d92a0efd49f3761e048ac09299230f
Rafael Souza <rafaelangical2@gmail.com>
2019-05-27 20:23:35 -0300

    Build files

--- android/app/src/main/java/com/caang_app/MainApplication.java
+++ android/app/src/main/java/com/caang_app/MainApplication.java
@@ -3,6 +3,8 @@ package com.caang_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNCWebViewPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
